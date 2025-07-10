7f995f9fe5455201fc57b8c8caabf6f92b5bfe4e
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-10 22:08:06 +0300

    player and level-1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { makePlayer } from "./entities";
 import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
@@ -34,6 +35,19 @@ async function gameSetup() {
     ]);
 
     k.add(level1Layout);
+
+    const kirb = makePlayer(
+      k,
+      level1SpawnPoints.player[0].x,
+      level1SpawnPoints.player[0].y
+    );
+
+    k.add(kirb);
+    k.camScale(k.vec2(0.7));
+    k.onUpdate(() => {
+      if (kirb.pos.x < level1Layout.pos.x + 432)
+        k.camPos(kirb.pos.x + 500, 800);
+    });
   });
 
   k.go("level-1");
